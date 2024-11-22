01706f9693488c91084a29b78366cf8f900c1723
pblieske <pio.blieske@bluewin.ch>
2024-11-22 15:14:16 +0100

    first draft

--- experiments/grid-search.py
+++ experiments/grid-search.py
@@ -43,26 +43,31 @@ n_x=200
 test_points=np.array([i / n_x for i in range(0, n_x)])
 y_true=functions_nonlinear(np.ndarray((n_x,1), buffer=test_points), data_args["beta"][0])
 
+#Choose the grid
 L=range(1, 50)                                  #Number of coefficients used
-lmb=np.array([2**i for i in range(-10, 0)])     #Regularization parameters
+Lmbd=np.array([2**i for i in range(-10, 0)])     #Regularization parameters
 
-err =np.empty(shape = [3,4]) 
+#Initialize matrix to save results
+err =np.empty(shape = [L.size, Lmbd.size]) 
 
-#Compute the basis
-basis_tmp = [np.cos(np.pi * test_points * k ) for k in range( L_temp)] 
-basis = np.vstack(basis_tmp).T
 #Get data
 data_values = get_data(n, **data_args, noise_var=noise_vars)
 u=data_values["u"]
 data_values.pop('u')
-#Estimate the function f
-estimates_decor = get_results(**data_values, **method_args, L=L_temp)
-estimates_fourrier= get_results(**data_values, method="ols", L=L_temp, a=0).T
-y_est=basis @ estimates_decor["estimate"]
-y_fourrier= basis @ estimates_fourrier
-y_est=np.ndarray((n_x, 1), buffer=y_est)
-#Compute the L^2-error
-print("$L^2$-error: ", 1/np.sqrt(n_x)*np.linalg.norm(y_true-y_est, ord=2))
+
+for l in L:
+    #Compute the basis
+    basis_tmp = [np.cos(np.pi * test_points * k ) for k in range( L_temp)] 
+    basis = np.vstack(basis_tmp).T
+    for j in range(0, Lmbd.size-1):
+         #Estimate the function f
+        estimates_decor = get_results(**data_values, **method_args, L=l)
+        estimates_fourrier= get_results(**data_values, method="ols", L=l, a=0).T
+        y_est=basis @ estimates_decor["estimate"]
+        y_fourrier= basis @ estimates_fourrier
+        y_est=np.ndarray((n_x, 1), buffer=y_est)
+        #Compute the L^2-error
+        err[l, j]=1/np.sqrt(n_x)*np.linalg.norm(y_true-y_est, ord=2)
 
 # ----------------------------------
 # plotting
@@ -97,68 +102,4 @@ plt.title(titles[data_args["process_type"]]
 
 plt.legend(handles=get_handles(), loc="lower left")
 plt.tight_layout()
-plt.show()
-
-
-#Plotting the confounder
-
-plt.plot(data_values['x'][sub], u[sub], 'o:w', mec=ibm_cb[4] )
-
-def get_handles():
-
-    point_1 = Line2D([0], [0], label='Confounder', marker='o', color='w', mec=ibm_cb[4], ls='')
-    return [point_1]
-
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Confounder")
-plt.legend(handles=get_handles(), loc="upper right")
-plt.hlines(0, 0, 1, colors='black', linestyles='dashed')
-plt.tight_layout()
-plt.show()
-
-# ----------------------------------
-# plotting the outliers
-# ----------------------------------
-
-trans=estimates_decor["tranformed"]
-P_n=trans["xn"]
-y_n=trans["yn"]
-
-#Get the sets of outliers
-inliniers=set(estimates_decor["inliniers"])
-n_true_outliers=max([int(round(data_args["fraction"]*len(data_args["band"]), ndigits=0)),1])
-true_outliers=set(range(0, n_true_outliers-1))
-detected_outliers=true_outliers.difference(inliniers)
-not_detected_outliers=true_outliers.difference(detected_outliers)
-true_intliniers=inliniers.difference(true_outliers)
-
-m_plots=np.ceil(L_temp/2).astype(int)
-fig, axs = plt.subplots(m_plots, 2)
-
-for l in range(0, L_temp):
-    i=np.floor(l/2).astype(int)
-    j=np.mod(l,2)
-    axs[i, j].plot(P_n[:, l], y_n, 'o:w', mec='black')
-    axs[i, j].plot(P_n[list(detected_outliers), l], y_n[list(detected_outliers)], 'o', color=ibm_cb[1])
-    axs[i, j].plot(P_n[list(not_detected_outliers), l], y_n[list(not_detected_outliers)], 'o', color=ibm_cb[4])
-    axs[i, j].plot(P_n[list(true_intliniers), l], y_n[list(true_intliniers)], 'o:w', mec=ibm_cb[0])
-    axs[i,j].axline((0,0), slope=estimates_decor["estimate"][l],color = 'black', linestyle = '--')
-    #Labels
-    axs[i, j].set_xlabel('$P(' + str(l) + ', : )$')
-    axs[i, j].set_ylabel('y')
-
-def get_handles():
-
-    point_1 = Line2D([0], [0], label='outliers found', marker='o', color=ibm_cb[1], ls='')
-    point_2 = Line2D([0], [0], label='outliers missed', marker='o', color=ibm_cb[4], ls='')
-    point_3 = Line2D([0], [0], label="inliniers found" , marker='o', mec=ibm_cb[0], color='w')
-
-    return [point_1, point_2, point_3]
-
-plt.suptitle("Detected Outliers")
-plt.tight_layout()
-fig.subplots_adjust(top=0.8)
-fig.legend(handles=get_handles(), loc="upper center", bbox_to_anchor=(0.55, 0.9), ncol=3)
-
 plt.show()
