4fcf64187cbd996c592e4e4b16b728903b42b9be
pblieske <pio.blieske@bluewin.ch>
2024-11-27 09:08:47 +0100

    added experiments_cross_validation.py

---
+++ experiments/experiments_cross_validation.py
@@ -0,0 +1,107 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+from utils_nonlinear import get_results, plot_results, get_data, plot_settings
+from synthetic_data import functions_nonlinear
+
+"""
+
+"""
+
+colors, ibm_cb = plot_settings()
+
+SEED = 1
+np.random.seed(SEED)
+random.seed(SEED)
+
+data_args = {
+    "process_type": "blpnl",       # "ou" | "blp" | "blpnl"
+    "basis_type": "cosine",     # "cosine" | "haar"
+    "fraction": 0.3,
+    "beta": np.array([2]),
+    "band": list(range(0, 50))  # list(range(0, 50)) | None
+}
+
+method_args = {
+    "a": 0.65,
+    "method": "torrent",        # "torrent" | "bfs"
+}
+
+m = 5   #Number of repetitions for the Monte Carlo
+noise_vars = 0
+methods=["torrent", "torrent_cv", "torrent_cv2"]
+num_data = [4 * 2 ** k for k in range(1, 12)]      # [4, 8, 10]
+
+# ----------------------------------
+# run experiments
+# ----------------------------------
+n_x=200     #Resolution of x-axis
+test_points = np.array([i / n_x for i in range(0, n_x)])
+y_true=functions_nonlinear(np.ndarray((n_x,1), buffer=test_points), data_args["beta"][0])
+
+for i in range(len(noise_vars)):
+    print("Noise Variance: ", noise_vars[i])
+    res = {"DecoR": [], "ols": []}
+    for n in num_data:
+        print("number of data points: ", n)
+        res["DecoR"].append([])
+        res["ols"].append([])
+        L_temp=max(np.floor(1/4*n**(1/2)).astype(int),1)
+        basis_tmp = [np.cos(np.pi * test_points * k ) for k in range(L_temp)]
+        basis = np.vstack(basis_tmp).T
+        print("number of coefficients: ", L_temp)
+
+        for _ in range(m):
+            data_values = get_data(n, **data_args, noise_var=noise_vars[i])
+            data_values.pop('u', 'basis')
+            estimates_decor = get_results(**data_values, **method_args, L=L_temp)
+            y_est=basis @ estimates_decor["estimate"]
+            y_est=np.ndarray((n_x, 1), buffer=y_est)
+
+            estimates_fourrier= get_results(**data_values, method="ols", L=L_temp, a=0).T
+            y_fourrier= basis @ estimates_fourrier
+            y_fourrier=np.ndarray((n_x, 1), buffer=y_fourrier)
+
+            res["DecoR"][-1].append(1/np.sqrt(n_x)*np.linalg.norm(y_true-y_est, ord=2))
+            res["ols"][-1].append(1/np.sqrt(n_x)*np.linalg.norm(y_true-y_fourrier, ord=2))
+            
+    res["DecoR"], res["ols"] = np.array(res["DecoR"]), np.array(res["ols"])
+    plot_results(res, num_data, m, colors=colors[i])
+
+# ----------------------------------
+# plotting
+# ----------------------------------
+
+titles = {"blp": "Band-Limited", "ou": "Ornstein-Uhlenbeck", "blpnl" : "Nonlinear: Band-Limited"}
+titles_basis = {"cosine": "", "haar": ", Haar basis"}
+titles_dim = {1: "", 2: ", 2-dimensional"}
+
+
+def get_handles():
+    point_1 = Line2D([0], [0], label='OLS', marker='o',
+                     markeredgecolor='w', color=ibm_cb[5], linestyle='-')
+    point_2 = Line2D([0], [0], label='DecoR', marker='X',
+                     markeredgecolor='w', color=ibm_cb[5], linestyle='-')
+    point_3 = Line2D([0], [0], label="$\sigma_{\eta}^2 = $" + str(noise_vars[0]), markersize=10,
+                     color=ibm_cb[1], linestyle='-')
+    point_4 = Line2D([0], [0], label="$\sigma_{\eta}^2 = $" + str(noise_vars[1]), markersize=10,
+                     color=ibm_cb[4], linestyle='-')
+    point_5 = Line2D([0], [0], label="$\sigma_{\eta}^2 = $" + str(noise_vars[2]), markersize=10,
+                     color=ibm_cb[2], linestyle='-')
+    return [point_1, point_2, point_3, point_4, point_5]
+
+
+plt.xlabel("number of data points")
+plt.ylabel("L^2 error")
+plt.title(titles[data_args["process_type"]]
+          + titles_basis[data_args["basis_type"]]
+          + titles_dim[len(data_args["beta"])])
+plt.xscale('log')
+plt.xlim(left=num_data[0] - 2)
+plt.hlines(0, num_data[0], num_data[-1], colors='black', linestyles='dashed')
+plt.legend(handles=get_handles(), loc="lower left")
+plt.tight_layout()
+
+plt.show()
