89c5bec04594884a8330176d6a00cd0e667b946d
pblieske <pio.blieske@bluewin.ch>
2025-03-16 15:27:00 +0100

    update documentation

--- experiments/confidence_intervals.py
+++ experiments/confidence_intervals.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from tqdm import tqdm
 from robust_deconfounding.utils import  get_funcbasis
-from utils_nonlinear import get_results, get_data, get_conf, bootstrap, plot_settings, double_bootstrap, conf_clip
+from utils_nonlinear import get_results, get_data, get_conf, bootstrap, plot_settings, double_bootstrap
 from synthetic_data import functions_nonlinear
 
 """
-    We run a simulation study to test how the confidence intervals proposed in the thesis perform, in particualr we want to investigate their coverage.
-    There are 4 methods tested, the two analytical versions using the all obersvations or only the estimated inliers of the transfromed sample, bootstraping and double bootstraping.
-    The experiment can take several hours to run, therefore the values are alread saved. To rerun the experiment, set the "run_exp" variable to True.
+    We run a simulation study to test how the confidence intervals proposed in the thesis perform, in particular, we want to investigate their coverage.
+    There are 4 methods tested: The two analytical versions using the all observations or only the estimated inliers of the transformed sample, bootstrapping and double bootstrapping.
+    The experiment can take several hours to run, therefore the values are already saved. To rerun the experiment, set the "run_exp" variable to True.
     The underlying true function can be selected over the variable "f".
 """
 
 run_exp=False      # Set to True for running the whole experiment and False to plot an experiment which was already run
-f="sigmoid"           # "sine" | "sigmoid", the underlying true function
+f="sine"           # "sine" | "sigmoid", the underlying true function
 
 # ----------------------------------
 # parameters
@@ -41,7 +41,7 @@ method_args = {
 }
 
 m = 200                                         # Number of Monte Carlo samples to draw
-L = max(np.floor(1/4*n**(1/2)).astype(int),2)     # number of basis functions
+L = max(np.floor(1/4*n**(1/2)).astype(int),2)   # number of basis functions
 
 colors, ibm_cb = plot_settings()                                    # import colors for plotting
 path_results=os.path.join(os.path.dirname(__file__), "results/")    # Path to the results
@@ -66,7 +66,9 @@ cov_l, cov_b, cov_db, cov_h=np.full([n_alpha, n_x], np.nan) , np.full([n_alpha, 
 if run_exp:
     # Initialize arrays to compute coveragess
     T_l,T_b, T_db,T_h = np.full([n_alpha, m, n_x], np.nan), np.full([n_alpha, m, n_x], np.nan), np.full([n_alpha, m, n_x], np.nan), np.full([n_alpha, m, n_x], np.nan)
+    
     for _ in tqdm(range(m)):
+       
         # Generate a sample and fit DecoR
         data_values = get_data(n, **data_args, noise_var=noise_vars)
         data_values.pop('u') 
@@ -79,15 +81,14 @@ if run_exp:
         B=200   # number of samples drawn for the bootstrap
 
         # Perform double bootstraping to estimate the acutal coverage levels
-        #cov_double=double_bootstrap(x_test=test_points, transformed=estimates_decor["transformed"], estimate=estimates_decor["estimate"], a=method_args["a"], L=L, basis_type=method_args["basis_type"], M=M, B=B)
+        cov_double=double_bootstrap(x_test=test_points, transformed=estimates_decor["transformed"], estimate=estimates_decor["estimate"], a=method_args["a"], L=L, basis_type=method_args["basis_type"], M=M, B=B)
         
         # Boostrapping
         boot=bootstrap(x_test=test_points, transformed=estimates_decor["transformed"], a=method_args["a"], L=L, basis_type=method_args["basis_type"], M=B)
-        #n_double=len(cov_double['nominal'])
+        n_double=len(cov_double['nominal'])
 
         for i in range(n_alpha):
 
-            """
             # Get the adjuste alpha from the double bootstrap. This is done for every point seperately since the coverage can differ.
             ci_db=np.full([n_x,2], np.nan)
             for j in range(n_x):
@@ -95,14 +96,11 @@ if run_exp:
                 ind_alpha=np.min([ind_h+1, int(B/2)-1])
                 alpha_boot=cov_double['nominal'][ind_alpha]
                 ci_db[j,:]=np.array([2*y_est[j]-boot[int(np.ceil((1+alpha_boot)/2*B)),j], 2*y_est[j]-boot[int(np.floor((1-alpha_boot)/2*B)),j]])
-            """
-            ci_db=conf_clip(x=test_points, **estimates_decor, L=L, alpha=alpha[i], basis_type=method_args["basis_type"], a=0.75)
 
             # Compute the confidence intervals
             ci_h=get_conf(x=test_points, **estimates_decor, L=L, alpha=alpha[i], basis_type=method_args["basis_type"], w=0)
             ci_b=np.stack(([2*y_est-boot[int(np.ceil((1+alpha[i])/2*B)),:], 2*y_est-boot[int(np.floor((1-alpha[i])/2*B)),:]]), axis=-1)
             ci_l=get_conf(x=test_points, **estimates_decor, L=L, alpha=alpha[i], basis_type=method_args["basis_type"], w=1)
-            #ci_h=conf_clip(x=test_points, **estimates_decor, L=L, alpha=alpha[i], basis_type=method_args["basis_type"], a=0.75)
 
             # Check if true the function f is contained
             T_h[i,_,:]=(ci_h[:,1]>=y_true[:,0]) & (y_true[:,0]>=ci_h[:, 0])
@@ -115,7 +113,7 @@ if run_exp:
          cov_l[i, :], cov_b[i,:], cov_db[i,:], cov_h[i,:]=np.sum(T_l[i, :, :], axis=0)/m, np.sum(T_b[i, :, :], axis=0)/m, np.sum(T_db[i, :, :], axis=0)/m, np.sum(T_h[i,:,:], axis=0)/m
 
     #Save the results using a pickle file
-    with open(path_results+"confidence_interval_" + str(data_args["beta"])+ '.pkl', 'wb') as fp:
+    with open(path_results+"confidence_interval_t" + str(data_args["beta"])+ '.pkl', 'wb') as fp:
             cov={'cov_l': cov_l, 'cov_b': cov_b, 'cov_db': cov_db, 'cov_h': cov_h}
             pickle.dump(cov, fp)
 
@@ -159,5 +157,5 @@ def get_handles():
 
 fig.subplots_adjust(right=10)
 fig.legend(handles=get_handles(), loc='outside right center')
-#plt.tight_layout(rect=[0, 0, 0.89, 1.0])
+plt.tight_layout(rect=[0, 0, 0.89, 1.0])
 plt.show()
